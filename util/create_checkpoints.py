c47f7f3f7d54f03e43ca368b3b8cdd9e767bde10
Sicong Zhuang <zhuangsc1989@gmail.com>
2013-08-22 12:04:01 +0200

    Basic preparations

--- util/create_checkpoints.py
+++ util/create_checkpoints.py
@@ -9,11 +9,10 @@ import sys
 cwd = os.getcwd()
 qemu_bin = '%s/qemu/qemu-system-x86_64' % cwd
 # qemu_img = '/home/avadh/workspace/vm/spec2006_2.qcow2'
-qemu_img = '/home/avadh/workspace/vm/splash_4core_orig.qcow2'
-# qemu_img = '/var/work/vm/splash_4core.qcow2'
-vm_memory = 4096
+qemu_img='/home/zhuang/Experiment/spec06-quantal.qcow2'
+vm_memory = 2048
 qemu_cmd = ''
-vm_smp = 4
+vm_smp = 1
 
 def add_to_cmd(opt):
     global qemu_cmd
@@ -28,7 +27,7 @@ add_to_cmd('-vnc :20')
 add_to_cmd('-cpu core2duo')
 
 # Add Image at the end
-# add_to_cmd('-hda %s' % qemu_img)
+#add_to_cmd('-hda %s' % qemu_img)
 add_to_cmd('-drive file=%s,cache=unsafe' % qemu_img)
 
 # Checkpoint list
@@ -38,7 +37,7 @@ check_list = []
 parsec_bench_list = ['blackscholes', 'bodytrack', 'ferret', 'freqmine',
         'swaptions', 'fluidanimate', 'vips', 'x264', 'canneal', 'dedup',
         'streamcluster', 'facesim', 'raytrace']
-# parsec_bench_list = ['facesim', 'raytrace']
+#parsec_bench_list = ['facesim', 'raytrace']
 
 pre_setup_str = '''cd parsec-2.1 ; . env.sh
         export PARSEC_CPU_NUM=`grep processor /proc/cpuinfo | wc -l`; echo $PARSEC_CPU_NUM
@@ -212,185 +211,202 @@ for bench in splash_bench_tilera_paper_specifics:
 # SPEC 2006 Benchmarks
 # Directory structure :
 # $HOME/spec2006/[compile_config]/[bench_name]/[executables]
-
+spec_list_min = [
+        {'name' : 'quantum', #20
+         'command' :
+         '''cd spec06/462.libquantum/run*
+         ~/create_checkpoint quantum; ../libquantum* 33 5 ; ~/stop_sim
+         '''
+         },
+		]
 spec_list = [
         {'name' : 'perl', # 0
          'command' :
-         '''cd spec2006bin/icc.fast/400.perlbench/
-         ~/create_checkpoint perl; ./perlbench_base.marss.icc.fast -I. -I./lib pack.pl ; ~/stop_sim
+         '''cd spec06/400.perlbench/run*
+         ~/create_checkpoint perl; ../perl* -I. -I./lib pack.pl ; ~/stop_sim
          '''
          },
         {'name' : 'bzip', # 1
          'command' :
-         '''cd spec2006bin/icc/401.bzip2/
-         ~/create_checkpoint bzip; ./bzip2_base.marss.icc.core2 input.program 2 ; ~/stop_sim
+         '''cd spec06/401.bzip2/run*
+         ~/create_checkpoint bzip; ../bzip2* input.program 5 ; ~/stop_sim
          '''
          },
         {'name' : 'gcc', # 2
          'command' :
-         '''cd spec2006bin/icc/403.gcc/
-         ~/create_checkpoint gcc; ./gcc_base.marss.icc.core2 cccp.i -o cccp.s ; ~/stop_sim
+         '''cd spec06/403.gcc/run*
+         ~/create_checkpoint gcc; ../gcc* cccp.i -o cccp.s ; ~/stop_sim
          '''
          },
         {'name' : 'bwaves', # 3
          'command' :
-         '''ulimit -s unlimited; cd spec2006bin/icc/410.bwaves/
-         ~/create_checkpoint bwaves; ./bwaves_base.marss.icc.core2 ; ~/stop_sim
+         '''ulimit -s unlimited; cd spec06/410.bwaves/run*
+         ~/create_checkpoint bwaves; ../bwaves* ; ~/stop_sim
          '''
          },
         {'name' : 'gamess', # 4
          'command' :
-         '''cd spec2006bin/icc/416.gamess/
-         ~/create_checkpoint gamess; ./gamess_base.marss.icc.core2 < exam29.config ; ~/stop_sim
+         '''cd spec06/416.gamess/run*
+         ~/create_checkpoint gamess; ../gamess* < exam29.config ; ~/stop_sim
          '''
          },
         {'name' : 'mcf', # 5
          'command' :
-         '''cd spec2006bin/icc/429.mcf/
-         ~/create_checkpoint mcf; ./mcf_base.marss.icc.core2 inp.in ; ~/stop_sim
+         '''cd spec06/429.mcf/run*
+         ~/create_checkpoint mcf; ../mcf* inp.in ; ~/stop_sim
          '''
          },
         {'name' : 'milc', # 6
          'command' :
-         '''cd spec2006bin/gcc/433.milc/
-         ~/create_checkpoint milc; ./milc_base.marss.gcc.core2 < su3imp.in ; ~/stop_sim
+         '''cd spec06/433.milc/run*
+         ~/create_checkpoint milc; ../milc* < su3imp.in ; ~/stop_sim
          '''
          },
         {'name' : 'zeusmp', # 7
          'command' :
-         '''ulimit -s unlimited; cd spec2006bin/icc/434.zeusmp/
-         ~/create_checkpoint zeusmp; ./zeusmp_base.marss.icc.core2 ; ~/stop_sim
+         '''ulimit -s unlimited; cd spec06/434.zeusmp/run*
+         ~/create_checkpoint zeusmp; ../zeusmp* ; ~/stop_sim
          '''
          },
         {'name' : 'gromacs', # 8
          'command' :
-         '''cd spec2006bin/gcc/435.gromacs/
-         ~/create_checkpoint gromacs ; ./gromacs_base.marss.gcc.core2 -silent -deffnm gromacs -nice 0 ; ~/stop_sim
+         '''cd spec06/435.gromacs/run*
+         ~/create_checkpoint gromacs ; ../gromacs* -silent -deffnm gromacs -nice 0 ; ~/stop_sim
          '''
          },
         {'name' : 'cactusADM', # 9
          'command' :
-         '''cd spec2006bin/gcc/436.cactusADM/
-         ~/create_checkpoint cactusADM; ./cactusADM_base.marss.gcc.core2 benchADM.par ; ~/stop_sim
+         '''cd spec06/436.cactusADM/run*
+         ~/create_checkpoint cactusADM; ../cactusADM* benchADM.par ; ~/stop_sim
          '''
          },
         {'name' : 'leslie3d', #10
          'command' :
-         '''ulimit -s unlimited; cd spec2006bin/icc/437.leslie3d/
-         ~/create_checkpoint leslie3d; ./leslie3d_base.marss.icc.core2 < leslie3d.in ; ~/stop_sim
+         '''ulimit -s unlimited; cd spec06/437.leslie3d/run*
+         ~/create_checkpoint leslie3d; ../leslie3d* < leslie3d.in ; ~/stop_sim
          '''
          },
         {'name' : 'namd', #11
          'command' :
-         '''cd spec2006bin/icc/445.namd/
-         ~/create_checkpoint namd; ./namd_base.marss.icc.core2  --input namd.input --iterations 1 --output namd.out ; ~/stop_sim
+         '''cd spec06/444.namd/run*
+         ~/create_checkpoint namd; ../namd*  --input namd.input --iterations 1 --output namd.out ; ~/stop_sim
          '''
          },
         {'name' : 'gobmk', #12
          'command' :
-         '''cd spec2006bin/icc/445.gobmk/
-         ~/create_checkpoint gobmk; ./gobmk_base.marss.icc.core2 --quiet --mode gtp < capture.tst ; ~/stop_sim
+         '''cd spec06/445.gobmk/run*
+         ~/create_checkpoint gobmk; ../gobmk* --quiet --mode gtp < capture.tst ; ~/stop_sim
          '''
          },
         {'name' : 'deal2', #13
          'command' :
-         '''cd spec2006bin/gcc/447.dealII/
-         ~/create_checkpoint deal2; ./dealII_base.marss.gcc.core2 2 ; ~/stop_sim
+         '''cd spec06/447.dealII/run*
+         ~/create_checkpoint deal2; ../dealII* 2 ; ~/stop_sim
          '''
          },
         {'name' : 'soplex', #14
          'command' :
-         '''cd spec2006bin/icc/450.soplex/
-         ~/create_checkpoint soplex; ./soplex_base.marss.icc.core2 test.mps ; ~/stop_sim
+         '''cd spec06/450.soplex/run*
+         ~/create_checkpoint soplex; ../soplex* -m10000 test.mps ; ~/stop_sim
          '''
          },
         {'name' : 'povray', #15
          'command' :
-         '''cd spec2006bin/icc/453.povray/
-         ~/create_checkpoint povray; ./povray_base.marss.icc.core2 SPEC-benchmark-test.ini ; ~/stop_sim
+         '''cd spec06/453.povray/run*
+         ~/create_checkpoint povray; ../povray* SPEC-benchmark-test.ini ; ~/stop_sim
          '''
          },
         {'name' : 'calculix', #16
          'command' :
-         '''cd spec2006bin/gcc/454.calculix/
-         ~/create_checkpoint calculix; ./calculix_base.marss.gcc.core2 beampic ; ~/stop_sim
+         '''cd spec06/454.calculix/run*
+         ~/create_checkpoint calculix; ../calculix* -i beampic ; ~/stop_sim
          '''
          },
         {'name' : 'hmm', #17
          'command' :
-         '''cd spec2006bin/icc/456.hmmer/
-         ~/create_checkpoint hmm; ./hmmer_base.marss.icc.core2 --fixed 0 --mean 325 --num 4500 --sd 200 --seed 0 bombesin.hmm ; ~/stop_sim
+         '''cd spec06/456.hmmer/run*
+         ~/create_checkpoint hmm; ../hmmer* --fixed 0 --mean 325 --num 4500 --sd 200 --seed 0 bombesin.hmm ; ~/stop_sim
          '''
          },
         {'name' : 'sjeng', #18
          'command' :
-         '''cd spec2006bin/icc/458.sjeng/
-         ~/create_checkpoint sjeng; ./sjeng_base.marss.icc.core2 test.txt ; ~/stop_sim
+         '''cd spec06/458.sjeng/run*
+         ~/create_checkpoint sjeng; ../sjeng* test.txt ; ~/stop_sim
          '''
          },
         {'name' : 'GemsFDTD', #19
          'command' :
-         '''ulimit -s unlimited; cd spec2006bin/icc/459.GemsFDTD/
-         ~/create_checkpoint GemsFDTD; ./GemsFDTD_base.marss.icc.core2 ; ~/stop_sim
+         '''ulimit -s unlimited; cd spec06/459.GemsFDTD/run*
+         ~/create_checkpoint GemsFDTD; ../GemsFDTD* ; ~/stop_sim
          '''
          },
         {'name' : 'quantum', #20
          'command' :
-         '''cd spec2006bin/icc/462.libquantum/
-         ~/create_checkpoint quantum; ./libquantum_base.marss.icc.core2 33 5 ; ~/stop_sim
+         '''cd spec06/462.libquantum/run*
+         ~/create_checkpoint quantum; ../libquantum* 33 5 ; ~/stop_sim
          '''
          },
         {'name' : 'h264', #21
          'command' :
-         '''cd spec2006bin/icc/464.h264ref/
-         ~/create_checkpoint h264; ./h264ref_base.marss.icc.core2 -d foreman_test_encoder_baseline.cfg ; ~/stop_sim
+         '''cd spec06/464.h264ref/run*
+         ~/create_checkpoint h264; ../h264ref* -d foreman_test_encoder_baseline.cfg ; ~/stop_sim
          '''
          },
         {'name' : 'tonto', #22
          'command' :
-         '''cd spec2006bin/icc/465.tonto/
-         ~/create_checkpoint tonto; ./tonto_base.marss.icc.core2 ; ~/stop_sim
+         '''cd spec06/465.tonto/run*
+         ~/create_checkpoint tonto; ../tonto* ; ~/stop_sim
          '''
          },
         {'name' : 'lbm', #23
          'command' :
-         '''cd spec2006bin/icc/470.lbm/
-         ~/create_checkpoint lbm; ./lbm_base.marss.icc.core2 20 reference.dat 0 1 100_100_130_cf_a.of ; ~/stop_sim
+         '''cd spec06/470.lbm/run*
+         ~/create_checkpoint lbm; ../lbm* 20 reference.dat 0 1 100_100_130_cf_a.of ; ~/stop_sim
          '''
          },
         {'name' : 'omnetpp', #24
          'command' :
-         '''cd spec2006bin/icc/471.omnetpp/
-         ~/create_checkpoint omnetpp; ./omnetpp_base.marss.icc.core2 omnetpp.ini ; ~/stop_sim
+         '''cd spec06/471.omnetpp/run*
+         ~/create_checkpoint omnetpp; ../omnetpp* omnetpp.ini ; ~/stop_sim
          '''
          },
         {'name' : 'astar', #25
          'command' :
-         '''cd spec2006bin/icc/473.astar/
-         ~/create_checkpoint astar; ./astar_base.marss.icc.core2 lake.cfg ; ~/stop_sim
+         '''cd spec06/473.astar/run*
+         ~/create_checkpoint astar; ../astar* lake.cfg ; ~/stop_sim
          '''
          },
-        {'name' : 'sphinx', #26
+		{'name' : 'wrf',  #26
          'command' :
-         '''cd spec2006bin/icc/482.sphinx3/
-         ~/create_checkpoint sphinx; ./sphinx_livepretend_base.marss.icc.core2 ctlfile . args.an4 ; ~/stop_sim
+         '''cd spec06/481.wrf/run*
+         ~/create_checkpoint wrf; ../wrf* ; ~/stop_sim
+         '''
+		 },
+        {'name' : 'sphinx', #27
+         'command' :
+         '''cd spec06/482.sphinx3/run*
+         ~/create_checkpoint sphinx; ../sphinx* ctlfile . args.an4 ; ~/stop_sim
          '''
          },
-        {'name' : 'xalanc', #27
+        {'name' : 'xalanc', #28
          'command' :
-         '''cd spec2006bin/gcc/483.xalancbmk/
-         ~/create_checkpoint xalanc; ./Xalan_base.marss.gcc.core2 -v test.xml xalanc.xsl ; ~/stop_sim
+         '''cd spec06/483.xalancbmk/run*
+         ~/create_checkpoint xalanc; ../Xalan* -v test.xml xalanc.xsl ; ~/stop_sim
          '''
          },
         ]
 
 # To create single checkpoint
 #check_list.append(parsec_list[6])
+#for package in parsec_roi_list:
+#	check_list.append(package)
 #check_list.append(parsec_roi_list[6])
-check_list = splash_list
+#check_list = splash_list
+#print check_list
 
 # To create all spec checkpoints
-# check_list = splash_list
+check_list = spec_list
+print(check_list)
 
 print("Execution command: %s" % qemu_cmd)
 print("Number of Chekcpoints to create: %d" % len(check_list))
